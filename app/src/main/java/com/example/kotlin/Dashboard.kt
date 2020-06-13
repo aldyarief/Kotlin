227f81f0224fe7a61687283b12baf3f115cadcf6
aldryarief <aldreva@gmail.com>
2020-06-13 15:48:33 +0700

    dashboard

--- app/src/main/java/com/example/kotlin/Dashboard.kt
+++ app/src/main/java/com/example/kotlin/Dashboard.kt
@@ -1,12 +1,32 @@
 package com.example.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Dashboard : AppCompatActivity() {
+    var name: String? = null
+    var pass:kotlin.String? = null
+    var userid:kotlin.String? = null
+    var user:kotlin.String? = null
+    var barang:kotlin.String? = null
+    var beli:kotlin.String? = null
+    var jual:kotlin.String? = null
+    var koreksi:kotlin.String? = null
+    var laporan:kotlin.String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        name = intent.getStringExtra("name")
+        pass = intent.getStringExtra("pass")
+        userid = intent.getStringExtra("userid")
+        user = intent.getStringExtra("user")
+        barang = intent.getStringExtra("barang")
+        beli = intent.getStringExtra("beli")
+        jual = intent.getStringExtra("jual")
+        koreksi = intent.getStringExtra("koreksi")
+        laporan = intent.getStringExtra("laporan")
     }
 }
--- app/src/main/java/com/example/kotlin/Login.kt
+++ app/src/main/java/com/example/kotlin/Login.kt
@@ -45,7 +45,7 @@ class Login : AppCompatActivity() {
         textname = findViewById<View>(R.id.textname) as EditText
         textpass = findViewById<View>(R.id.textpass) as EditText
         password = findViewById<View>(R.id.checkBox1) as CheckBox
-        server_url = "http://aldry.agustianra.my.id/Login.php"
+        server_url = "http://aldry.agustianra.my.id/nitip/Login.php"
         pd = ProgressDialog(this)
 
         button!!.setOnClickListener {
